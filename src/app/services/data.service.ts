0df6490c569465efdceb3f5bf384f66df1a78e11
Franz Luepke <fluepke@rccl.com>
2022-07-13 18:21:11 -0500

    frontend data services migration to backend

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,23 +1,18 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
 import { Response } from "../models/response";
 import { User } from "../models/user";
-import * as env from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class DataService {
+  url = "http://127.0.0.1:8091";
+
   constructor(private httpClient: HttpClient) {}
 
   liveSearch(type: string, value: string) {
-    const headers = {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
-      'Authorization': 'Basic ' + btoa(`${env.environment.auth.username}:${env.environment.auth.password}`)
-  }
-  let field = '';
+    let field = '';
     if (type === 'firstName') {
       field = 'FIRST_NAME';
     } else if (type === 'lastName') {
@@ -25,60 +20,55 @@ export class DataService {
     } else {
       field = '_all';
     }
+    const headers = {
+      'Content-Type': 'application/json',
+    }
     const body = {
-      "query" : {
-        "field": field,
-        "prefix": value
-      },
-      "size": 10,
-      "from": 0,
-      "fields" : ["*"],
-      "explain": false,
-      "highlight": {}
+      field,
+      value,
     };
-    console.log(body);
-    return this.httpClient.post<Response>(env.environment.endpoint, JSON.stringify(body), { headers });
+    return this.httpClient.post<Response>(`${this.url}/live-search`, JSON.stringify(body), { headers });
   }
 
   manualSearch(user: User) {
     const headers = {
-      'Access-Control-Allow-Origin': '*',
       'Content-Type': 'application/json',
-      'Authorization': 'Basic ' + btoa(`${env.environment.auth.username}:${env.environment.auth.password}`)
+    }
+    const fields: {"field": string, "match": string}[] = [];
+    if (user?.consumerId) {
+      fields.push({
+        "field": "CNSMR_ID",
+        "match": user?.consumerId
+      });
+    }
+    if (user?.phone) {
+      fields.push({
+        "field": "CNSMR_PHONE_NBR",
+        "match": user?.phone
+      });
+    }
+    if (user?.email) {
+      fields.push({
+        "field": "EMAIL_ADDRESS",
+        "match": user?.email
+      });
+    }
+    if (user?.firstName) {
+      fields.push({
+        "field": "FIRST_NAME",
+        "match": user?.firstName
+      });
+    }
+    if (user?.lastName) {
+      fields.push({
+        "field": "LAST_NAME",
+        "match": user?.lastName
+      });
     }
     const body = {
-      "query" : {
-        "disjuncts": [
-          {
-            "field": "CNSMR_ID",
-            "match": user?.consumerId
-          },
-          {
-            "field": "CNSMR_PHONE_NBR",
-            "match": user?.phone
-          },
-          {
-            "field": "EMAIL_ADDRESS",
-            "match": user?.email
-          },
-          {
-            "field": "FIRST_NAME",
-            "match": user?.firstName
-          },
-          {
-            "field": "LAST_NAME",
-            "match": user?.lastName
-          }
-        ]
-      },
-      "size": 10,
-      "from": 0,
-      "fields" : ["*"],
-      "explain": false,
-      "highlight": {}
+      fields: fields,
     };
-    console.log(body);
-    return this.httpClient.post<Response>(env.environment.endpoint, JSON.stringify(body), { headers });
+    return this.httpClient.post<Response>(`${this.url}/manual-search`, JSON.stringify(body), { headers });
   }
 
   getMoreDetails(id: string) {
@@ -87,6 +77,6 @@ export class DataService {
     }
     const body = { id };
     console.log(body);
-    return this.httpClient.post<Response>("http://127.0.0.1:8088/user-detail", JSON.stringify(body), { headers });
+    return this.httpClient.post<Response>(`${this.url}/user-detail`, JSON.stringify(body), { headers });
   }
 }
